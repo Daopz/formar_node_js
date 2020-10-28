c7b03662706229f33292847856d2ec576b8146ff
Daopz <ingdoliveros@gmail.com>
2020-10-27 21:09:52 -0300

    Tercer cambio

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
 
     // Respond with "hello world" for requests that hit our root "/"
     app.get("/", function (req, res) {
-     return res.send("Hello Formar");
+     return res.send("Hello World");
     });
 
     // listen to port 7000 by default
