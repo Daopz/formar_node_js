15a10269e9729d7a07fb66ef51c1d63f4eea2842
Daopz <ingdoliveros@gmail.com>
2020-10-27 22:01:48 -0300

    Agregar Hello world 2

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ const request = require("supertest");
 
     describe("GET /", () => {
       it("respond with Hello World via Github Actions", (done) => {
-        request(app).get("/").expect("Hello World", done);
+        request(app).get("/").expect("Hello World via Github Actions", done);
       })
     });
